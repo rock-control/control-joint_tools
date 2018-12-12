6e604cc92e4be19182fe6c9951824c1ff90a74c9
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2018-12-12 13:37:44 -0200

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace joint_tools;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <joint_tools/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    joint_tools::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
